3aabce2030046c628a7c7f7871886ef7fce3256e
Kimberley Szelong <kimszelong@Kimberleys-MacBook-Air.local>
2018-08-05 14:15:42 +0100

    Header animation WIP

--- _clients/parkrun.markdown
+++ _clients/parkrun.markdown
@@ -2,7 +2,5 @@
 title: parkrun
 date: 2018-07-21 07:02:00 Z
 logo: "/uploads/parkrun.png"
-Work:
-- - parkrun
 ---
 
--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
 
-// BORDER
+	// BORDER
 	function borders() {
 		var sectionTop = $('#border-top-trigger').offset().top;
 
@@ -27,7 +27,102 @@ $( document ).ready(function() {
 
 	borders();
 
-// VID HOVER
+
+	// HEADER COLOUR SWAP 
+	function loop(){
+
+		// Detect request animation frame
+		var scroll = window.requestAnimationFrame
+			|| window.webkitRequestAnimationFrame
+			|| window.mozRequestAnimationFrame
+			|| window.msRequestAnimationFrame
+			|| window.oRequestAnimationFrame
+			// IE Fallback, you can even fallback to onscroll
+			|| function(callback){ window.setTimeout(callback, 1000/60) };
+
+		var lastPosition = -1;
+
+		// my Variables
+		var lastSection = false;
+		var replaceItemTop = -1;
+		var replaceItemBottom = -1;
+		var replaceItemHeight = -1;
+		var top = window.pageYOffset;
+
+	  	// my sections to calculate stuff
+	  	var sections = document.querySelectorAll('.section');
+	  	var replaceContainer = document.querySelectorAll('.js-replace');
+	  	var replaceItem = document.querySelectorAll('.js-replace__item');
+
+	  	if (replaceItem.length > 0) {
+	    	// get top position of item from container, because image might not have loaded
+			replaceItemTop = parseInt(replaceContainer[0].getBoundingClientRect().top);
+		    replaceItemHeight = replaceItem[0].offsetHeight;
+		    replaceItemBottom = replaceItemTop + replaceItemHeight;
+		}
+
+		var sectionTop = -1;
+		var sectionBottom = -1;
+		var currentSection = -1;
+		  
+		  // Fire when needed
+		if (lastPosition == window.pageYOffset) {
+		    scroll(loop);
+		    return false;
+		} else {
+		    lastPosition = window.pageYOffset;
+
+			// Your Function
+			Array.prototype.forEach.call(sections, function(el, i){
+			    sectionTop = parseInt(el.getBoundingClientRect().top);
+			    sectionBottom = parseInt(el.getBoundingClientRect().bottom);
+
+		    // active section
+		    if ( (sectionTop <= replaceItemBottom) && (sectionBottom > replaceItemTop)) {
+		      
+				// check if current section has bg 
+				currentSection = el.classList.contains('section--bg');
+
+				// switch class depending on background image
+				if ( currentSection ) { 
+					replaceContainer[0].classList.remove('js-replace--reverse');
+				} else {
+					replaceContainer[0].classList.add('js-replace--reverse')
+				}
+		    }
+		    // end active section
+
+		    if ((sectionTop <= replaceItemBottom) && (sectionBottom > replaceItemTop)) {
+		    }
+
+		    // if active Section hits replace area
+		    //KIM: FALSE UNLESS LOGO GOING OVER SECTION EDGE
+		    if ( (replaceItemTop < sectionTop) && ( sectionTop <= replaceItemBottom) ) {
+				// animate only, if section background changed
+				if (currentSection != lastSection)  {
+					document.documentElement.style.setProperty('--replace-offset', 100 / replaceItemHeight * parseInt(sectionTop - replaceItemTop) + '%');
+				}
+		    }
+		    // end active section in replace area
+
+		    // if section above replace area
+		    if ( replaceItemTop >= sectionTop ) {
+				// set offset to 0 if you scroll too fast
+				document.documentElement.style.setProperty('--replace-offset', 0 + '%');
+				// set last section to current section
+				lastSection = currentSection;
+
+		    }
+
+		}); 
+	}
+
+	scroll( loop )
+	}
+
+	loop();
+
+	// VID HOVER
 	function videoHover() {
 		var figure = $(".video-wrapper").hover( hoverVideo, hideVideo );
 
@@ -42,10 +137,21 @@ $( document ).ready(function() {
 		}
 	};
 
-
 	videoHover();
 
-// BARBA
+
+	// NAVIGATION 
+	function openNav() {
+		$("#nav").click(function() {
+			$("#nav").toggleClass( 'is-active');
+		});
+	};
+
+	openNav();
+
+
+
+	// BARBA
 
  //    var transEffect = Barba.BaseTransition.extend({
  //        start: function(){
@@ -74,6 +180,18 @@ $( document ).ready(function() {
     Barba.Dispatcher.on('newPageReady', function() {
 		borders();
 		videoHover();
+		openNav();
+		loop();
 	});
 
+
+	// WINDOW RESIZE
+
+
+	window.onresize = function(event) {
+		loop();
+		borders();
+	};
+
 });
+
